b0130ab946473b1c40b10a3c65386e7ddf10ddb0
TessierV <5628@holbertonstudents.com>
2024-01-17 09:06:15 -0800

    ex 11

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,35 @@
+function countPrimeNumbers() {
+    const isPrime = i => {
+        if (i <= 1) return false;
+        for (let j = 2; j * j <= i; j++) {
+            if (i % j === 0) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const start = performance.now();
+
+    let count = 0;
+    for (let j = 2; j <= 100; j++) {
+        if (isPrime(j)) {
+            count++;
+        }
+    }
+
+    const end = performance.now();
+    return end - start;
+}
+
+
+function runCountPrimeNumbersMultipleTimes(times) {
+    let result = 0;
+
+    for (let i = 0; i < times; i++) {
+        result += countPrimeNumbers();
+    }
+    setTimeout(() => {console.log(`Execution time of calculating prime numbers ${times} times was ${result} milliseconds.`)}, 0)
+}
+
+runCountPrimeNumbersMultipleTimes(100);

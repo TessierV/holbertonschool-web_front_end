dd115f2ca0a540073e70a0c2c9cb815a47b322c9
TessierV <5628@holbertonstudents.com>
2024-01-17 10:22:03 -0800

    test

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,22 @@
+function createElement(data) {
+    var paragraph = document.createElement('p');
+    paragraph.textContent = data; // 'textcontent' should be 'textContent'
+    document.body.appendChild(paragraph);
+}
+
+function queryWikipedia(callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(xhttp.responseText); // 'xhr' should be 'xhttp'
+            const pageId = Object.keys(response.query.pages)[0];
+            const extract = response.query.pages[pageId].extract;
+            callback(extract);
+        }
+    };
+    xhttp.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*", true);
+    xhttp.send();
+}
+
+queryWikipedia(createElement);
+

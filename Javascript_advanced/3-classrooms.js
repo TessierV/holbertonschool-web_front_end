0aebff9dd875b719465a0970185ff1e7f496db0a
TessierV <5628@holbertonstudents.com>
2024-01-17 02:15:14 -0800

    exercice 3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,22 @@
+function createClassRoom (numberOfStudents){
+
+    function studentSeat(seat){
+        return function() {
+            return seat;
+        };
+    }
+
+    var students = [];
+
+    for (var iteration = 0; iteration < numberOfStudents; iteration++ ){
+		students.push(studentSeat(iteration + 1));
+    }
+
+    return students;
+}
+
+var classRoom = createClassRoom(10);
+
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
